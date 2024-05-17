ef7e0ad3dfc21b2487f5fe1f4adc368bff9c59fc
Trhe_piernas <hruizdeegino@gmail.com>
2024-05-17 13:10:07 +0200

    DashboardKontrolatzailea aldatu entregatzeko dauden paketen datu eta laister entregatzeko paketen datuak erakusteko,baiata ere tablan erakusten diren datuak hurrengo 10 egunetan egin behr diren entregetara aldatu

--- src/main/java/paketak/admin/kontrolatzaileak/DashboardKontrolatzailea.java
+++ src/main/java/paketak/admin/kontrolatzaileak/DashboardKontrolatzailea.java
@@ -1,5 +1,7 @@
 package paketak.admin.kontrolatzaileak;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import paketak.admin.modeloak.Banatzailea;
 import paketak.admin.modeloak.Paketea;
 import paketak.admin.zerbitzuak.MysqlConector;
@@ -10,8 +12,13 @@ import java.util.ArrayList;
 public class DashboardKontrolatzailea {
 
     private static MysqlConector mysql = MysqlConector.getInstance();
+    @FXML
+    public Label banatzekopaketeak;
+    @FXML
+    public Label laisterBanatzekoak;
 
     public void initialize(){
+        aktualizatuPaketeDatuak();
     }
 
     public void showPanel(){
@@ -21,7 +28,9 @@ public class DashboardKontrolatzailea {
     public static ArrayList<Paketea> getPaketeak(){
         ArrayList<Paketea> zerrenda = new ArrayList<Paketea>();
 
-        ResultSet emaitza = mysql.createQuery("SELECT * FROM `Paketea`");
+        ResultSet emaitza = mysql.createQuery("SELECT * FROM `Paketea` \n" +
+                "WHERE `entrega_egin_beharreko_data` BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 10 DAY) \n" +
+                "ORDER BY `entrega_egin_beharreko_data` ASC;");
 
         try {
 
@@ -43,5 +52,32 @@ public class DashboardKontrolatzailea {
         }
 
         return zerrenda;
+    }
+
+    /**
+     * Funtzio honek datu basean dauden pakete kopurua eta laister banatu behar diren pakete kopurua aktulizatzen du interfazean
+     */
+    public void aktualizatuPaketeDatuak(){
+        ResultSet paketeak = mysql.createQuery("SELECT COUNT(*) FROM `Paketea` WHERE `Banatzailea_id` IS NULL");
+
+        try {
+            if (paketeak.next()) {
+                int count = paketeak.getInt(1);
+                banatzekopaketeak.setText(Integer.toString(count));
+            }
+            ResultSet banatzekoak = mysql.createQuery("SELECT COUNT(*) FROM `Paketea` \n" +
+                    "WHERE `entrega_egin_beharreko_data` BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 10 DAY) \n" +
+                    "ORDER BY `entrega_egin_beharreko_data` ASC;");
+
+            if (banatzekoak.next()) {
+                int count = banatzekoak.getInt(1);
+                laisterBanatzekoak.setText(Integer.toString(count));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
