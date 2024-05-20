4292890c8c46c016149cfb136200ba07f416ad05
Unax Azpirotz <1ag3.unaxazpi@tolosaldealh.eus>
2024-05-20 13:55:55 +0200

    Banatzaileak panel

--- src/main/java/paketak/admin/Main.java
+++ src/main/java/paketak/admin/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("pakAG Kudeatzailea - Administrazioa");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/paketak/admin/irudiak/icon.png")));
         stage.setScene(scene);
         stage.show();
     }
