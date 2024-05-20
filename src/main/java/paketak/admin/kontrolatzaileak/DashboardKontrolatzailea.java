ae274a0e328a48b406a4fb5f7a2677e6f7ed0e60
Unax Azpirotz <1ag3.unaxazpi@tolosaldealh.eus>
2024-05-20 13:37:40 +0200

    Banatzaileak panel

--- src/main/java/paketak/admin/kontrolatzaileak/DashboardKontrolatzailea.java
+++ src/main/java/paketak/admin/kontrolatzaileak/DashboardKontrolatzailea.java
@@ -3,11 +3,13 @@ package paketak.admin.kontrolatzaileak;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import paketak.admin.modeloak.Paketea;
 import paketak.admin.zerbitzuak.MysqlConector;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
@@ -17,6 +19,15 @@ public class DashboardKontrolatzailea {
 
     @FXML
     private VBox datuPanela;
+
+    @FXML
+    private Button btHasiera;
+
+    @FXML
+    private Button btBanatzaileak;
+
+    @FXML
+    private Button btPaketeak;
 
     public void initialize(){
 
